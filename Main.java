5cd9f07ced9f21f85d2a02b17d06d90bba239038
PoluektovaDarya <123874713+PoluektovaDarya@users.noreply.github.com>
2023-09-15 13:17:07 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,36 @@
+import java.util.Random;
+
+public class Sequential_search {
+    public static void main(String[] args) {
+        int[] numbers = new int[1000];
+        Random random = new Random();
+        int max = 0;
+        int max_2 = 0;
+        int max_7 = 0;
+        int max_14 = 0;
+
+        for(int number : numbers){
+            number = random.nextInt(10000);
+            if (number > max) {
+                max = number;
+            }
+            if ((number % 2 == 0) && (number > max_2)) {
+                max_2 = number;
+            }
+            if ((number % 7 == 0) && (number > max_7)) {
+                max_7 = number;
+            }
+            if ((number % 14 == 0) && (number > max_14)) {
+                max_14 = number;
+            }
+        }
+        int prod_max = max * max_14;
+        int prod_max2 = max_2 * max_7;
+        if (prod_max == 0 && prod_max2 == 0){
+            System.out.println(-1);
+        }
+        else{
+            System.out.println(Math.max(prod_max, prod_max2));
+        }
+    }
+}
